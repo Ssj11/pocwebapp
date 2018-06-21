19ca73c4f8fccc685b211fc49d88dd91307c4503
divyam <divyam.vyas@capgemini.com>
2018-06-21 17:22:21 +0530

    New Change

---
+++ src/test/java/pocwebapp/SelinumTest.java
@@ -0,0 +1,13 @@
+package pocwebapp;
+
+import org.testng.annotations.Test;
+
+public class SelinumTest {
+
+	@Test
+	public void Jenkins()
+	{
+		System.out.println("Welcome To Jenkins Selinum Test");
+	}
+	
+}
